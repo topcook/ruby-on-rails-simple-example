5f451e365f84a50122ebfb01c87f921f5b13f7fb
topcook <codecooker1996@gmail.com>
2022-08-22 01:42:38 -0300

    added routing params id

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "articles#index"
   
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+
+  # get "/articles/create", to: "articles#create"
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
